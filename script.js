aea4708068f6f544bb37c00fd24b53286e8647bd
Sofia-Hassan <isofiahassan06@gmail.com>
2025-12-24 21:21:09 +0500

    Add user input method.
    size of square changed according to input.

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 const divGrid = document.querySelector('.container')
+const userInput = document.querySelector('input')
+const btn = document.querySelector('.enter')
+
+btn.addEventListener('click',makeGrid)
 
 let opacity = 5
+let rows=5, colums = 6
 
-for(let i=0 ; i<20 ;i++){
+for(let i=0 ; i < rows*colums ;i++){
   const div = document.createElement('div')
   divGrid.appendChild(div)
 }
@@ -15,7 +20,6 @@ function changeColor(e){
 
 function random(){
   const random = Math.floor(Math.random() * 255 )
-  console.log(random)
   return random
 }
 
@@ -25,4 +29,28 @@ function color(){
     opacity = 5
   }
   return `rgba(${random()},111,112,${opacity}%)`
+}
+
+function makeGrid(){
+  for(let i=0 ; i < rows*colums ;i++){
+    divGrid.firstChild.remove()
+  }
+  let string = ''
+  string = userInput.value.split('x')
+  rows = Number(string[0])
+  colums = Number(string[1])
+  if((isNaN(rows) && isNaN(colums)) || rows==0 || rows>100 || colums==0 || colums>100 ){
+    alert('Enter a valid number in range from 1 to 100')
+  }
+  else{
+    if(isNaN(colums)){
+      colums = rows
+    }
+    for(let i=0 ; i < rows*colums ;i++){
+      const div = document.createElement('div')
+      div.style.width = (1000/rows).toString() + 'px'
+      div.style.height = (500/colums).toString() + 'px'
+      divGrid.appendChild(div)
+    }
+  }
 }
