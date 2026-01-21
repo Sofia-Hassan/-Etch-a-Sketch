6c506a00d0e839b90efd956e8e6d44ba5e4d4fd4
Sofia-Hassan <isofiahassan06@gmail.com>
2026-01-21 17:41:55 +0500

    Add sound effect and fix 'grid removing by invalid user input' error.

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # -Etch-a-Sketch
 
 This is a board with 30 squares initially. On hovering over their color is changed randomly.
+
+Sound Effect by [freesound_community] from [Pixabay]
+
+[freesound_community]: https://pixabay.com/users/freesound_community-46691455/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=45622
+
+[Pixabay]: https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=45622
--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const divGrid = document.querySelector('.container')
 const userInput = document.querySelector('input')
 const btn = document.querySelector('.enter')
+const soundEffect = new Audio('./sounds/water-drip.mp3')
 
 btn.addEventListener('click',makeGrid)
-console.log(userInput)
 userInput.addEventListener('keypress', e =>{
   if (e.key === 'Enter'){
     makeGrid()
@@ -18,7 +18,11 @@ for(let i=0 ; i < rows*colums ;i++){
   divGrid.appendChild(div)
 }
 
-divGrid.addEventListener('mouseover',e => changeColor(e) )
+divGrid.addEventListener('mouseover',e => {
+  changeColor(e)
+  soundEffect.play()
+  soundEffect.currentTime = 0
+})
 
 function changeColor(e){
   e.target.style.backgroundColor = color()
@@ -38,18 +42,20 @@ function color(){
 }
 
 function makeGrid(){
-  for(let i=0 ; i < rows*colums ;i++){
-    divGrid.firstChild.remove()
-  }
+  
   let string = ''
   string = userInput.value.toLowerCase()
-  string = string.split('x')
-  rows = Number(string[0])
-  colums = Number(string[1])
-  if((isNaN(rows) && isNaN(colums)) || rows==0 || rows>100 || colums==0 || colums>100 ){
+  string = string.split('x') // now string[0] holds rows and string[1] holds columns
+
+  if((isNaN(string[0]) && isNaN(string[1])) || string[0]==0 || string[0]>100 || string[1]==0 || string[1]>100 ){
     alert('Enter a valid number in range from 1 to 100')
   }
   else{
+    for(let i=0 ; i < rows*colums ;i++){
+      divGrid.firstChild.remove()
+    }
+      rows = string[0]
+      colums = string[1]
     if(isNaN(colums)){
       colums = rows
     }
